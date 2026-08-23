88a25b865933ad8d5af909710bfdeb39f8177df1
Francisco Molinet <f.molinethenriquez@uandresbello.edu>
2026-08-23 09:59:15 -0400

    Crear archivo JavaScript base

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Gestor de Actividades Académicas
+// Archivo principal de JavaScript
+
+// 'feature/registro' implementará la captura del formulario
+// 'feature/listado' implementará la visualización de actividades
+
+console.log("Gestor de Actividades cargado correctamente.");
